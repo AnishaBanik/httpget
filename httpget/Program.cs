dfe96f8a904e3f9d64c8679ed0561775377297a6
Anisha Banik <anisha@codeiam.com>
2020-10-17 12:27:49 +0530

    first commit

---
+++ httpget/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientEx
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient req = new HttpClient();
+            //req.
+            var content = await req.GetAsync("https://httpbin.org/get");
+            Console.WriteLine(await content.Content.ReadAsStringAsync());
+            Console.WriteLine(content.StatusCode);
+
+        }
+    }
+}
